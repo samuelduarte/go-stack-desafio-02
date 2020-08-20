6cbf1725e6e7273f1129d13d9d48c33cc92dd523
Samuel DuarteD <sduarteft@gmail.com>
2020-08-20 16:41:24 -0300

    post and get methods

--- src/app.js
+++ src/app.js
@@ -13,23 +13,59 @@ const repositories = [];
 
 app.get("/repositories", (request, response) => {
 
-  console.log('entrou');
-  // TODO
+
+
+    return response.json(repositories);  
+  
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+
+   const {title, url, techs} = request.body;
+   
+   const repository = {id: uuid(), title: title,url: url, techs: techs};
+
+        repositories.push(repository);
+
+   return response.json({repository});
+
 });
 
 app.put("/repositories/:id", (request, response) => {
+
+  const { id } = request.params;
+
+  const {title, url, techs} = request.body;
+
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id);
+
+  const repository = [
+    title,
+    url,
+    techs
+  ]
+
+
+  return response.json({repository});
+
+
+
+
+
   // TODO
 });
 
 app.delete("/repositories/:id", (request, response) => {
+
+    const { id } = request.params;
+
+
   // TODO
 });
 
 app.post("/repositories/:id/like", (request, response) => {
+
+  const { id } = request.params;
   // TODO
 });
 
